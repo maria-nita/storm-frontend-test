353efa7772d2503637d0a6601e5385c34336dfc2
Maria <maria-nita@live.com>
2020-02-12 16:50:57 +0000

    setting up the project and taking a look at the api data

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
 {
 	console && console.log('%c careers@stormid.com ', 'background: #272727; color: #ffffff');
 }
+
+fetch('http://localhost:4000/api/task')
+	.then((response) => {
+		console.log(response.json());
+		return response.json();
+	})
