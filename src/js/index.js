72332a190f59e3f73458523b399a647a2597ce8f
Maria <maria-nita@live.com>
2020-02-15 16:08:01 +0000

    task titles rendered to page

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,35 @@
-{
-	console && console.log('%c careers@stormid.com ', 'background: #272727; color: #ffffff');
+import axios from 'axios';
+
+var apiTasks;
+
+const elements = {
+	taskList: document.querySelector('.task-list')
 }
 
-fetch('http://localhost:4000/api/task')
-	.then((response) => {
-		console.log(response.json());
-		return response.json();
-	})
+async function getData() {
+	try {
+		const resultAPI = await axios('http://localhost:4000/api/task');
+		const data = resultAPI.data;
+		return data;
+	} catch(error) {
+		alert(error);
+	}
+}
+
+function renderTitle(value) {
+	const markup = `<h2>${value}</h2>`;
+	elements.taskList.insertAdjacentHTML('beforeend', markup);
+}
+
+getData().then(data => {
+	apiTasks = data;
+	if (apiTasks) {
+		apiTasks.forEach(function(task) {
+			if (task.title) {
+				renderTitle(task.title);
+			}
+		});
+	}
+});
+
+
