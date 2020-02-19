d6b8d70aac6b6da0b05087341ba1dcbe9a2909ba
Maria <maria-nita@live.com>
2020-02-19 10:48:58 +0000

    Tidying up a few things

--- src/js/views/taskListView.js
+++ src/js/views/taskListView.js
@@ -3,8 +3,8 @@ import { elements } from './base';
 export const renderTasks = (item, status, priority) => {
 	const markup = `
 	<div class="task-list__task task-list__task--priority-${item.importance}" data-api-id="${item.id}">
-		<input type="checkbox" id="${item.title}" name="${item.title}" ${status}>
-		<label for="${item.title}">${item.title} (Priority: ${priority})</label>
+		<input type="checkbox" id="${formatName(item.title)}" name="${formatName(item.title)}" ${status}>
+		<label for="${formatName(item.title)}">${item.title} (Priority: ${priority})</label>
 		<button type="button" class="task-list__delete-task">Delete</button>
 	</div>
 	`;
@@ -16,4 +16,10 @@ export const clearTasks = () => {
 	if (tasks.length >= 1) {
 		tasks.forEach(task => task.parentElement.removeChild(task));
 	}
+}
+
+function formatName(value) {
+	var valueSeparated = value.split(' ');
+	var valueReconstructed = valueSeparated.join('-');
+	return valueReconstructed;
 }
